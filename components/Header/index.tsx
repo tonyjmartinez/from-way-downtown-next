dda2b4644c32af466688366a672b2456be5b8fdf
Tony Martinez <ajosephmartinez@gmail.com>
2020-07-06 11:33:51 -0700

    Remove chakra

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,55 +1,7 @@
 import React from "react";
-import {
-  Box,
-  Heading,
-  Flex,
-  Text,
-  Button,
-  useColorMode,
-} from "@chakra-ui/core";
-import Drawer from "../Drawer";
-
-const MenuItems = ({ children }) => (
-  <Text mt={{ base: 4, md: 0 }} mr={6} display="block">
-    {children}
-  </Text>
-);
 
 const Header = (props) => {
-  const { colorMode, toggleColorMode } = useColorMode();
-
-  const bgColor = { light: "white", dark: "gray.800" };
-  const color = { light: "black", dark: "white" };
-
-  return (
-    <Flex
-      as="nav"
-      align="center"
-      justify="space-between"
-      wrap="wrap"
-      padding="1.5rem"
-      bg={bgColor[colorMode]}
-      color={color[colorMode]}
-      style={{
-        position: "fixed",
-        bottom: 0,
-        width: "100%",
-      }}
-      {...props}
-    >
-      <Flex align="center" mr={5}>
-        <Heading as="h1" size="lg" letterSpacing={"-.1rem"}>
-          From Way Downtown
-        </Heading>
-      </Flex>
-
-      <Button onClick={toggleColorMode}>
-        {colorMode === "light" ? "Dark" : "Light"}
-      </Button>
-
-      <Drawer />
-    </Flex>
-  );
+  return <div>Header</div>;
 };
 
 export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,8 @@
 import Head from "next/head";
 import { withApollo } from "../utils/withApollo";
-import { Button, useColorMode } from "@chakra-ui/core";
 
 const Home = (props) => {
-  const { colorMode, toggleColorMode } = useColorMode();
-  console.log("props", props);
-  console.log("colormode", colorMode);
-
-  const toggleDark = () => {
-    console.log("toggle");
-    toggleColorMode();
-  };
-
-  return (
-    <>
-      <a href="/api/login">Login</a>
-      <Button onClick={toggleDark}>
-        Toggle {colorMode === "light" ? "Dark" : "Light"}
-      </Button>
-    </>
-  );
+  return <>Home</>;
 };
 
 export default withApollo()(Home);
